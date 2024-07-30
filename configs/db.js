2dd60e84a29330eab87aa54818c3ef9fdf820de0
Thanh Nhan <npthanhnhan2003@gmail.com>
2024-07-30 12:58:47 +0700

    config template engine

--- configs/db.js
+++ configs/db.js
@@ -1,14 +1,18 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+const dbname = process.env.MONGO_DBNAME;
+
+const uri = `mongodb+srv://${username}:${password}@cluster0.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected');
+    await mongoose.connect(uri, {});
+    console.log('Connected to MongoDB');
   } catch (err) {
-    console.error(err.message);
+    console.error('Failed to connect to MongoDB', err);
     process.exit(1);
   }
 };
