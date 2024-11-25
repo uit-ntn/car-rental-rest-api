7be34dae5ca2380ffef57f8b6e54ba3f457d0761
Thanh Nhan <npthanhnhan2003@gmail.com>
2024-11-25 23:39:37 +0700

    CRUD functionality for Car model

--- routes/car.js
+++ routes/car.js
@@ -2,13 +2,10 @@ const express = require('express');
 const router = express.Router();
 const carController = require('../controllers/carController');
 
-// Add Car
-router.post('/', carController.addCar);
-
-// Get Car Details
-router.get('/:id', carController.getCarById);
-
-// List Available Cars
-router.get('/', carController.getAllCars);
+router.post('/cars', carController.createCar);          // Create a car
+router.get('/cars', carController.getAllCars);          // Get all cars
+router.get('/cars/:id', carController.getCarById);      // Get a car by ID
+router.put('/cars/:id', carController.updateCar);       // Update a car by ID
+router.delete('/cars/:id', carController.deleteCar);    // Delete a car by ID
 
 module.exports = router;
