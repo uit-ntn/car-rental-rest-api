7cbfbf01a9e7177376c98c7145fca641ad15d611
Thanh Nhan <npthanhnhan2003@gmail.com>
2024-12-25 23:35:16 +0700

    update mail sender

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login } = require('../controllers/authController');
+const { signup, login, forgotPassword, resetPassword } = require('../controllers/authController');
 
 router.post('/signup', signup);
 router.post('/login', login);
-
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password', resetPassword);
 
