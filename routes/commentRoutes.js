6947b192576c46ef0fb3f155e59df3fa41e3a7f9
Thanh Nhan <npthanhnhan2003@gmail.com>
2025-03-03 23:46:04 +0700

    fix comment route

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -6,5 +6,5 @@ const router = express.Router();
 router.post('/', commentController.createComment);          // Create a comment
 router.get('/', commentController.getAllComments);          // Get all comments
 router.get('/:car_id', commentController.getCommentsByCarId); // Get comments by car_id
-router.delete("/:comment_id",commentController.deleteComment); // Delete commment by id
+router.delete("/:id",commentController.deleteComment); // Delete commment by id
 module.exports = router;
